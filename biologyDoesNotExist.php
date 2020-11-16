72bf2cd65791e0b04857a355de4bdd71619586e6
Skittlez1522 <61472875+Skittlez1522@users.noreply.github.com>
2020-11-16 11:12:54 -0700

    Add files via upload

---
+++ biologyDoesNotExist.php
@@ -0,0 +1,15 @@
+<?php
+  include_once 'header.php';
+ ?>
+
+    <div class="main">
+      <div class="container">
+        <h2>Why doesn't biology exist?</h2>
+        <p>There are multiple reasons biology doesn't exist, but the main reason this "science" doesn't exist is because it is just a subscience of physics. Now, you may say, "That doesn't mean it can't exist," and this is where you're wrong:</p>
+        <h4>In all honesty, I don't know why it doesn't exist, but just go with it please?</h4>
+      </div>
+    </div>
+
+<?php
+  include_once 'footer.php';
+ ?>
---
+++ cellStructureAndFunction.php
@@ -0,0 +1,50 @@
+<?php
+  include_once 'header.php';
+?>
+
+    <div class="main">
+      <div class="container">
+        <h2>Cell Organelle Analogy</h2>
+        <p>The structure of a cell and the structure of a city is very similar. Here's why:</p>
+        <dl>
+          <dt>Nucleus is like the city hall</dt>
+          <dd>- They both are the controlling forces of the cell/city.</dd>
+          <dt>Cytoplasm is like the atomsphere</dt>
+          <dd>- The main substance that takes up space throughout the city/cell.</dd>
+          <dt>Nuclear Envelope is like the city hall fence</dt>
+          <dd>- They both protect the control center, allowing materials in and out.</dd>
+          <dt>Nucleolus is like the office in the city hall</dt>
+          <dd>- They are the area where information is stored</dd>
+          <dt>Ribosomes are like the food-producing factories</dt>
+          <dd>- They both bring raw materials together.</dd>
+          <dt>Rough Endoplasmic Reticulum is like the streets</dt>
+          <dd>- Cars travel on streets like ribosomes travel by the Rough ER.</dd>
+          <dt>Smooth Endoplasmic Reticulum is like the manufacturing plants</dt>
+          <dd>- They both make proteins and fats that are packaged and sent off.</dd>
+          <dt>Golgi Apparatus is like the Post Office</dt>
+          <dd>- Things are sorted, packaged, and distributed around the cell/city.</dd>
+          <dt>Lysosomes are like the garbage disposals</dt>
+          <dd>- They both break down wastes and extra materials by disposing them.</dd>
+          <dt>Vacuoles are like warehouses</dt>
+          <dd>- They both store items and substances</dd>
+          <dt>Peroxisomes are like garbage men</dt>
+          <dd>- They both work to keep the city/cell clean.</dd>
+          <dt>Centriole is like a household.</dt>
+          <dd>- In both, information is passed down from parent to offspring.</dd>
+          <dt>Cilia is like the airport</dt>
+          <dd>- They transport cells/people around from place to place.</dd>
+          <dt>Mitochondria is like a power plant</dt>
+          <dd>- They both produce energy to give out to the cell/city.</dd>
+          <dt>Chloroplast are like solar energy plants</dt>
+          <dd>- They both capture energy from the sun.</dd>
+          <dt>The cell wall is a like a city wall</dt>
+          <dd>- They both support structure of the cell/city.</dd>
+          <dt>Cytoskeleton is like the superstructure of buildings</dt>
+          <dd>- They both give internal framework that creates shape in the cells/buildings</dd>
+        </dl>
+      </div>
+    </div>
+
+<?php
+  include_once 'footer.php';
+ ?>
